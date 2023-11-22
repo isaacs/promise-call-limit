f3defed6e010a3ba67fce27edbedb572eaf02afb
Gar <gar+gh@danger.computer>
2023-11-22 10:03:05 -0800

    feat: add rejectLate option, switch to opts
    BREAKING CHANGE: the function now expects an opts parameter
    
    PR-URL: https://github.com/isaacs/promise-call-limit/pull/12
    Credit: @wraithgar
    Close: #12
    Reviewed-by: @isaacs

--- test/basic.js
+++ test/basic.js
@@ -27,7 +27,7 @@ t.test('two by two', t => {
       calledTre = true
       res(3)
     }, 50)),
-  ], 2).then(res => t.strictSame(res, [1, 2, 3]))
+  ], { limit: 2 }).then(res => t.strictSame(res, [1, 2, 3]))
 })
 
 t.test('rejection', t => t.rejects(callLimit([
@@ -39,3 +39,28 @@ t.test('triple rejection', t => t.rejects(callLimit([
   () => Promise.reject(new Error('poop')),
   () => Promise.reject(new Error('poop')),
 ]), { message: 'poop' }))
+
+t.test('late rejection', async t => {
+  const results = []
+  await t.rejects(callLimit([
+    () => new Promise(resolve => setTimeout(() => {
+      results.push('first success')
+      resolve('ok')
+    }, 50)),
+    () => new Promise((_, reject) => {
+      setTimeout(() => {
+        results.push('slow rejection')
+        reject(new Error('slow rejection'))
+      }, 100)
+    }),
+    () => {
+      results.push('fast rejection')
+      return Promise.reject(new Error('fast rejection'))
+    },
+    () => new Promise(resolve => setTimeout(() => {
+      results.push('second success')
+      resolve('ok 2')
+    }, 50)),
+  ], { limit: 2, rejectLate: true }), { message: 'fast rejection' })
+  t.match(results, ['first success', 'fast rejection', 'slow rejection', 'second success'])
+})
